ce094db04d7e09a1c8fecf2f81cc0dc09bfd0ece
Hitesh Negi <hiteshnegi08@gmail.com>
2024-05-23 15:17:52 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,23 +1,18 @@
 from langchain.llms import GooglePalm
-from langchain.utilities import SQLDatabase #connecting to SQL database
+from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import FAISS #vector embeddngs
 from langchain.prompts import SemanticSimilarityExampleSelector
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 from langchain.prompts import FewShotPromptTemplate
 from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
 from langchain.prompts.prompt import PromptTemplate
-from dotenv import load_dotenv
 
 from few_shots import few_shots
-from decimal import Decimal
-import pandas as pd
 
 import os
-import re
-import ast
-
-load_dotenv() # take environment variables from .env (especially openai api key)
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
 
 
 def get_few_shot_db_chain():
@@ -26,24 +21,17 @@ def get_few_shot_db_chain():
     db_host = "localhost"
     db_name = "zeho_tshirts"
 
-    db = SQLDatabase.from_uri(
-        f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",
-        sample_rows_in_table_info=3,
-    )
+    db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",
+                              sample_rows_in_table_info=3)
     llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
-    #Embeddings
-    embeddings=GoogleGenerativeAIEmbeddings(model='models/embedding-001')
 
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
     to_vectorize = [" ".join(example.values()) for example in few_shots]
-    #generating a vector store: 
-    vectorstore = FAISS.from_texts(to_vectorize, embeddings, metadatas=few_shots)
-     #example selector to check sematic similarity
+    vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=few_shots)
     example_selector = SemanticSimilarityExampleSelector(
         vectorstore=vectorstore,
-        k=2, #number of examples
+        k=2,
     )
-
-    #Prompt prefix
     mysql_prompt = """You are a MySQL expert. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer to the input question.
     Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
     Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
@@ -59,29 +47,18 @@ def get_few_shot_db_chain():
     
     No pre-amble.
     """
-    #Example prompt format
+
     example_prompt = PromptTemplate(
-        input_variables=[
-            "Question",
-            "SQLQuery",
-            "SQLResult",
-            "Answer",
-        ],
+        input_variables=["Question", "SQLQuery", "SQLResult","Answer",],
         template="\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}",
     )
 
-    # Entire Prompt
     few_shot_prompt = FewShotPromptTemplate(
         example_selector=example_selector,
         example_prompt=example_prompt,
         prefix=mysql_prompt,
         suffix=PROMPT_SUFFIX,
-        input_variables=[
-            "input",
-            "table_info",
-            "top_k",
-        ],  # These variables are used in the prefix and suffix
+        input_variables=["input", "table_info", "top_k"], #These variables are used in the prefix and suffix
     )
-    chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, prompt=few_shot_prompt,return_direct=True,return_intermediate_steps=True)
+    chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, prompt=few_shot_prompt)
     return chain
-
